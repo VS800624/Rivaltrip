0666b3cb073568f7c2b5c9125e66a02aee059e90
VS800624 <vishalsingh800624@gmail.com>
2026-01-28 20:09:03 +0530

    Fetched popular-destination countires with api in home page

--- src/components/home/PopularDestination.jsx
+++ src/components/home/PopularDestination.jsx
@@ -1,9 +1,33 @@
 import { Form, Link } from "react-router-dom";
 import { popularCountries } from "../../utils/data";
 import Search from "./Search";
+import { BASE_URL } from "../../utils/constants";
+import { useEffect, useState } from "react";
+import Loading from "../Loading";
+import axios from "axios";
 
 
 const PopularDestination = () => {
+
+    const [popularDestinations, setPopularDestinations] = useState([]);
+
+    const fetchPopularDestinations = async(limit) => {
+        try{
+            const res = await axios.get(`${BASE_URL}/popular-destinations?limit=${limit}`)
+            // console.log("API RESPONSE:", res.data);
+            setPopularDestinations(res.data.popularCountries)
+        }catch(err){
+            console.error(err)
+        }
+    }
+    
+    useEffect(() => {
+        if(popularDestinations.length === 0){
+            fetchPopularDestinations(8)
+        }
+    }, [])  
+
+    
     return <> <section className="max-w-[1450px] mx-auto">
     <div className="flex flex-col items-center justify-center md:h-[720px] h-[680px]  mt-[0px] bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1732951360671-ed85cf569e8b?fm=jpg&q=60&w=2100&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">  
         {/* <img className="w-full h-full object-cover object-center" src="https://images.unsplash.com/photo-1732951360671-ed85cf569e8b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /> */}
@@ -17,15 +41,15 @@ const PopularDestination = () => {
      </Link>
     </div>
     <section className="grid sm:grid-cols-2  justify-content md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 md:mx-[80px] lg:mx-[100px] ">
-        {
-            popularCountries.map((property) => {
-                const {countryName, city, img, id} = property
-                return ( <div key={id} className=" rounded-lg bg-white md:w-[272px] w-[330px]  mx-auto md:mx-[0px] md:border-[1px] md:border-gray-200 mb-[40px] group overflow-hidden hover:shadow-md">
+        { popularDestinations.length ===0 ? <Loading /> : (
+            popularDestinations.map((destination) => {
+                const {countryName, city, img, _id, slug} = destination
+                return ( <div key={_id} className=" rounded-lg bg-white md:w-[272px] w-[330px]  mx-auto md:mx-[0px] md:border-[1px] md:border-gray-200 mb-[40px] group overflow-hidden hover:shadow-md">
                                 <div className="md:h-[380px] md:w-[272px] w-[330px] h-[480px] mx-auto md:mx-[0px]  overflow-hidden ">
                                     <img className="object-cover  object-center cursor-pointer h-full w-full transition-transform duration-300 ease-in-out active:scale-[1.2]  md:group-hover:scale-[1.1]" src= {img} alt={city} />
                                 </div>
                                 <div className="border-[1px] border-gray-200  pb-[20px] ">
-                                    <Link to={`/destination-info/${id.toLowerCase()}`}>
+                                    <Link to={`/destination-info/${slug}`}>
                                         <p className=" mt-[14px] mb-[14px] ml-[30px] items-center font-semibold cursor-pointer hover:text-blue-400 hover:transition-all hover:duration-300 hover:ease-in-out">
                                         <i className="fa-solid fa-location-dot mr-[5px]"></i>{city}, {countryName}         
                                         </p>
@@ -36,7 +60,7 @@ const PopularDestination = () => {
                             </div>
                         </div>
                 )
-            })
+            }))
         }
         
     </section> 
